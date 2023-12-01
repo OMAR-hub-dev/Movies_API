42231070f2fe3cd1e8e418300d8f0dc93039eda5
Oulaya omar <oulaya.omar@gmail.com>
2023-12-01 18:51:29 +0100

    frontend_react

---
+++ movie_frontend/src/api/axiosConfig.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+export default axios.create({
+    baseURL: 'https://9c96-103-106-239-104.ap.ngrok.io',
+    headers: {"ngork-skip-browser-warning": "true"}
+})
--- src/main/java/dev/omar/Movies/ReviewService.java
+++ src/main/java/dev/omar/Movies/ReviewService.java
@@ -8,13 +8,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
+    @Autowired
     private ReviewRepository reviewRepository;
     @Autowired
     private MongoTemplate mongoTemplate;
 
     public Review createReview(String reviewBody, String imdbId){
-        Review review = new Review(reviewBody);
-        reviewRepository.insert(review);
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
         mongoTemplate.update(Movie.class)
                 .matching(Criteria.where("imdbId").is(imdbId))
                 .apply(new Update().push("reviewIds").value(review))

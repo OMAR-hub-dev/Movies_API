fbedab156943619ff0cf02deaaa40f39d634e214
Oulaya omar <oulaya.omar@gmail.com>
2023-12-04 22:56:40 +0100

    not found

--- movie_frontend/src/App.js
+++ movie_frontend/src/App.js
@@ -7,6 +7,7 @@ import Home from './components/home/Home';
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
 import Reviews from './components/reviews/Reviews';
+import NotFound from './components/notFound/NotFound';
 
 function App() {
   const [movies, setMovies]= useState();
@@ -16,7 +17,7 @@ function App() {
   const getMovies = async ()=>{
                     try{
                       const response = await api.get("/api/v1/movies")
-                      console.log(response.data);
+                      // console.log(response.data);
                       setMovies(response.data);
                     }catch(err){
                       console.log(err);
@@ -45,6 +46,7 @@ function App() {
           <Route path="/" element={<Home movies= {movies} />}></Route>
           <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
           <Route path="/Reviews/:movieId" element ={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews} />}></Route>
+          <Route path="*" element={<NotFound/>}></Route>
         </Route>
       </Routes>
     </div>
